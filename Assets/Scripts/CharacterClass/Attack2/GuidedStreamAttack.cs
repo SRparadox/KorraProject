261aff2b41f54eef54860c1e840828bf55a619c7
Nick Corfmat <ncorfmat@ucsc.edu>
2025-01-31 14:09:36 -0800

    working stream prototype

--- Assets/Scripts/CharacterClass/Attack2/GuidedStreamAttack.cs
+++ Assets/Scripts/CharacterClass/Attack2/GuidedStreamAttack.cs
@@ -1,20 +1,26 @@
 using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GuidedStreamAttack: MonoBehaviour
 {
     [SerializeField] GuidedStream streamPrefab;
+    [SerializeField] Camera cam;
 
     public void Trigger()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit[] hits = Physics.RaycastAll(ray);
 
-        if (Physics.Raycast(ray, out hit))
+        foreach (RaycastHit hit in hits)
         {
-            SpawnGuidedStream(hit.point);
+            if (!hit.collider.CompareTag("Player"))
+            {
+                SpawnGuidedStream(hit.point);
+                break;
+            }
         }
     }
 
