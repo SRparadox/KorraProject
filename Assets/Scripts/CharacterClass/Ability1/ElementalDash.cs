7273e4e9407949ca0321c47655cbeddb0bdac6aa
Nick Corfmat <ncorfmat@ucsc.edu>
2025-01-31 16:50:09 -0800

    dash update

--- Assets/Scripts/CharacterClass/Ability1/ElementalDash.cs
+++ Assets/Scripts/CharacterClass/Ability1/ElementalDash.cs
@@ -6,7 +6,7 @@ public class ElementalDash: MonoBehaviour
     [SerializeField] private float dashSpeed = 20f;
     [SerializeField] private float dashDuration = 0.2f;
 
-    [SerializeField] ParticleSystem vfxTrail;
+    [SerializeField] private ParticleSystem vfxTrailPrefab; // Reference to the particle system prefab
 
     private CharacterController characterController;
 
@@ -22,16 +22,22 @@ public class ElementalDash: MonoBehaviour
 
     private IEnumerator Dash()
     {
+        // Instantiate the VFX trail prefab and set the parent to the character
+        ParticleSystem vfxTrail = Instantiate(vfxTrailPrefab, transform.position, Quaternion.identity);
+        vfxTrail.transform.SetParent(transform); // Set parent to the character
+
         vfxTrail.Play();
 
         float startTime = Time.time;
 
+        // Perform the dash while the duration is not finished
         while (Time.time < startTime + dashDuration)
         {
             characterController.Move(transform.forward * dashSpeed * Time.deltaTime);
             yield return null;
         }
 
+        Destroy(vfxTrail.gameObject, vfxTrail.main.duration);
         vfxTrail.Stop();
     }
 }
