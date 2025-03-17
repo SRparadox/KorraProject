8d20781348d0cf69904147096c4d19c8af5c4c7b
Nick Corfmat <ncorfmat@ucsc.edu>
2025-03-17 16:42:08 -0700

    UI update

---
+++ Assets/Code/Scripts/ParallaxBackground.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// code from ChatGPT
+
+public class ParrallaxBackground: MonoBehaviour
+{
+    public float parrallaxStrength = 0.1f;
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        mousePosition.x /= Screen.width;
+        mousePosition.y /= Screen.height;
+
+        Vector3 offset = new Vector3(
+            (mousePosition.x - 0.5f) * parrallaxStrength,
+            (mousePosition.y - 0.5f) * parrallaxStrength,
+            0);
+
+        transform.position = initialPosition + offset;
+    }
+}
