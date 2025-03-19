3e3f1fa7d1b139666a7240885484f7c8858ed10d
Nick Corfmat <ncorfmat@ucsc.edu>
2025-03-19 10:50:50 -0700

    updating playernetwork

--- Assets/Code/Scripts/Networking/PlayerNetwork.cs
+++ Assets/Code/Scripts/Networking/PlayerNetwork.cs
@@ -9,56 +9,88 @@ public class PlayerNetwork: NetworkBehaviour
     private ThirdPersonController thirdPersonController;
     private StarterAssetsInputs starterAssetsInputs;
     private PlayerInput playerInput;
-
     private CinemachineVirtualCamera[] virtualCameras;
+    private Transform cameraRoot;
 
     private void Awake()
     {
-        thirdPersonController = GetComponent<ThirdPersonController>();
-        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
-        playerInput = GetComponent<PlayerInput>();
+        TryGetComponent(out thirdPersonController);
+        TryGetComponent(out starterAssetsInputs);
+        TryGetComponent(out playerInput);
 
         virtualCameras = GetComponentsInChildren<CinemachineVirtualCamera>(true);
+        cameraRoot = transform.Find("PlayerCameraRoot");
     }
 
     public override void OnNetworkSpawn()
     {
-        if (!IsOwner)
+        if (IsOwner)
         {
-            Debug.Log($"[PlayerNetwork] {gameObject.name} is NOT the local player. Disabling components...");
-
-            if (starterAssetsInputs != null)
-                starterAssetsInputs.enabled = false;
-            if (thirdPersonController != null)
-                thirdPersonController.enabled = false;
-            if (playerInput != null)
-                playerInput.enabled = false;
-
-            foreach (var cam in virtualCameras)
-            {
-                cam.gameObject.SetActive(false);
-            }
-
-            Transform cameraRoot = transform.Find("PlayerCameraRoot");
-            if (cameraRoot != null)
-            {
-                cameraRoot.gameObject.SetActive(false);
-            }
+            EnableLocalPlayer();
         } else
         {
-            Debug.Log($"[PlayerNetwork] {gameObject.name} is the local player. Enabling components...");
-
-            if (starterAssetsInputs != null)
-                starterAssetsInputs.enabled = true;
-            if (thirdPersonController != null)
-                thirdPersonController.enabled = true;
-            if (playerInput != null)
-                playerInput.enabled = true;
-
-            foreach (var cam in virtualCameras)
-            {
-                cam.gameObject.SetActive(true);
-            }
+            EnableRemotePlayer();
         }
+    }
+
+    private void EnableLocalPlayer()
+    {
+        Debug.Log("SPAWNED LOCAL PLAYER");
+
+        // Enable player input and movement for the local player
+        starterAssetsInputs.enabled = true;
+        thirdPersonController.enabled = true;
+        playerInput.enabled = true;
+
+        // Activate cameras
+        foreach (var cam in virtualCameras)
+        {
+            cam.gameObject.SetActive(true);
+        }
+
+        if (cameraRoot != null)
+            cameraRoot.gameObject.SetActive(true);
+    }
+
+    private void EnableRemotePlayer()
+    {
+        Debug.Log("SPAWNED REMOTE PLAYER");
+
+        // Disable player input and cameras for remote players
+        playerInput.enabled = false;
+        foreach (var cam in virtualCameras)
+        {
+            cam.gameObject.SetActive(false);
+        }
+
+        if (cameraRoot != null)
+            cameraRoot.gameObject.SetActive(false);
+
+        // Allow movement simulation on the server for remote clients
+        bool shouldEnableMovement = IsServer;
+        thirdPersonController.enabled = shouldEnableMovement;
+        starterAssetsInputs.enabled = shouldEnableMovement;
+    }
+
+    private void Update()
+    {
+        if (!IsOwner)
+            return;
+
+        // Send player input to the server
+        SubmitInputServerRpc(starterAssetsInputs.move, starterAssetsInputs.jump, starterAssetsInputs.sprint);
+    }
+
+    [ServerRpc]
+    private void SubmitInputServerRpc(Vector2 moveInput, bool jump, bool sprint)
+    {
+        // Ensure the server does not overwrite owner-controlled movement
+        if (!IsOwner)
+            return;
+
+        // Sync input values for movement simulation on the server
+        starterAssetsInputs.move = moveInput;
+        starterAssetsInputs.jump = jump;
+        starterAssetsInputs.sprint = sprint;
     }
 }
