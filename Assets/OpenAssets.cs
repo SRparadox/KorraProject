4a2dd6965b79464d02445048c545cb71aa3d6c71
Nick Corfmat <ncorfmat@ucsc.edu>
2025-03-21 18:54:19 -0700

    localized credits and tutorial page

---
+++ Assets/OpenAssets.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class OpenLink: MonoBehaviour
+{
+    public void OpenWebsite()
+    {
+        Application.OpenURL("https://docs.google.com/spreadsheets/d/1PnWRL0XDSJIiIZNlhPeM2eMmgFryn4Ch9yPPPojjOAE/edit?usp=sharing");
+    }
+}
